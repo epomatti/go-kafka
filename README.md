48eb65281ebbf422fda3eb6e8d4c42183b97f02e
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-05-05 18:50:51 -0300

    compose

--- README.md
+++ README.md
@@ -1,19 +1,35 @@
 # Go Kafka demo
 
+### Local consumer
+
 Start Kafka:
 
 ```
-docker-compose up -d
+docker-compose -f docker-compose-kafkaonly.yaml up -d
 ```
 
-Create the topic:
+Create the `demo` topic:
 
 ```
-docker exec broker kafka-topics --bootstrap-server broker:9092 --create --topic quickstart
+docker exec broker kafka-topics --bootstrap-server broker:9092 --create --topic demo
 ```
 
 Start the consumer:
 
 ```
 go run .
+```
+
+Use the CLI producer to send inline events to Kafka:
+
+```
+docker exec --interactive --tty broker kafka-console-producer --bootstrap-server broker:9092 --topic demo
+```
+
+### Docker consumer
+
+
+
+```
+docker-compose -f docker-compose-full.yaml up
 ```
--- main.go
+++ main.go
@@ -1,9 +1,56 @@
 package main
 
 import (
-	_ "github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 func main() {
+
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "broker:9092",
+		"group.id":          "foo",
+		"auto.offset.reset": "earliest"})
+
+	if err != nil {
+		fmt.Printf("Failed to create consumer: %s", err)
+		os.Exit(1)
+	}
+
+	topic := "demo"
+
+	err = c.SubscribeTopics([]string{topic}, nil)
+	if err != nil {
+		fmt.Printf("Failed to subscribe to topics: %s", err)
+		os.Exit(1)
+	}
+	// Set up a channel for handling Ctrl-C, etc
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Process messages
+	run := true
+	for run {
+		select {
+		case sig := <-sigchan:
+			fmt.Printf("Caught signal %v: terminating\n", sig)
+			run = false
+		default:
+			ev, err := c.ReadMessage(100 * time.Millisecond)
+			if err != nil {
+				// Errors are informational and automatically handled by the consumer
+				continue
+			}
+			fmt.Printf("Consumed event from topic %s: key = %-10s value = %s\n",
+				*ev.TopicPartition.Topic, string(ev.Key), string(ev.Value))
+		}
+	}
+
+	c.Close()
 
 }
